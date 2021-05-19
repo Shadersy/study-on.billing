fa7d88ca43ba62c3222e3c3799dd8beda52375e8
Shadersy <shadersy96@mail.ru>
2021-05-19 12:40:47 +0300

    adding all files

---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/api/doc.json' => [[['_route' => 'app.swagger', '_controller' => 'nelmio_api_doc.controller.swagger'], null, ['GET' => 0], null, false, false, null]],
+        '/api/v1/register' => [[['_route' => 'register', '_controller' => 'App\\Controller\\AuthController::register'], null, ['POST' => 0], null, false, false, null]],
+        '/api' => [[['_route' => 'api', '_controller' => 'App\\Controller\\AuthController::api'], null, null, null, false, false, null]],
+        '/api/v1/auth' => [[['_route' => 'login_check'], null, ['POST' => 0], null, false, false, null]],
+        '/api/doc' => [[['_route' => 'app.swagger_ui', '_controller' => 'nelmio_api_doc.controller.swagger_ui'], null, ['GET' => 0], null, false, false, null]],
+        '/' => [[['_route' => 'main', '_controller' => 'App\\Controller\\AuthController::main'], null, ['GET' => 0], null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
